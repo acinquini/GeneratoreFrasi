d1b994681b013192560c8915027f4ca2d82ba4c7
Alessandro <acinquini@gmail.com>
2022-04-15 09:09:37 +0200

    Third commit

--- Generator.py
+++ Generator.py
@@ -17,9 +17,7 @@ def readData(L, filename):
 # Funzione che sceglie casualmente un articolo ma coerente
 # con il genere e numero del sostantivo passato
 def randomArticle(genre, name):
-
     vowels = ['a','e','i','o','u']
-
     if (genre == 's.f.'):
         if (name[0] in vowels):
             A_filtered = ["l'","un'"]
@@ -44,21 +42,26 @@ def randomArticle(genre, name):
               (name[0] == 'g') and (name[1] == 'n') or
               (name[0] == 'p') and (name[1] == 's') or
               (name[0] == 'x') or  (name[0] == 'y') or  (name[0] == 'z')):
-            A_filtered = ["gli'","degli"]
+            A_filtered = ["gli","degli"]
         else:
             A_filtered = ["i","dei"]
     else:
-        raise Exception ('GenreNumberError')
+        raise Exception ('GenreNumberError ' + genre)
     return(r.choice(A_filtered))
 
 # Funzione per costruzione casuale frase tipo:
-# "Il cane mangia la mela"
-#  A1  N1  V1(tr) A2 N2
-def randomSentenceType1(words, verbs):
+# "Il cane allegro mangia la mela matura"
+#  A1  N1  Q1      V1(tr) A2 N2    Q2
+def randomSentenceType1(words, verbs, adjectives):
     # Selezione casuale nome
     N1_dict = r.choice(words)
     N1 = N1_dict['Parola']
     genre = N1_dict['GenereNumero']
+
+    # Qualificazione nome con aggettivo
+    Q_filtered = [q for q in adjectives if (q['GenereNumero'] == genre)]
+    Q1 = r.choice(Q_filtered)['Parola']
+    Q_filtered.clear()
 
     # Selezione filtrata da genere e numero articolo
     A1 = randomArticle(genre, N1)
@@ -74,16 +77,23 @@ def randomSentenceType1(words, verbs):
     N2 = N2_dict['Parola']
     genre =  N2_dict['GenereNumero']
 
+    # Qualificazione nome con aggettivo
+    Q_filtered = [q for q in adjectives if (q['GenereNumero'] == genre)]
+    Q2 = r.choice(Q_filtered)['Parola']
+    Q_filtered.clear()
+
     # Selezione filtrata da genere e numero articolo
     A2 = randomArticle(genre, N2)
 
     # Costruzione casuale frase in base a struttura
-    return(assemble(A1,N1,V1,A2,N2))
+    return(assemble(A1,N1,Q1,V1,A2,N2,Q2))
 
 # Funzione per costruzione casuale frase tipo:
-# "Mentre il cane mangia la pecora corre"
-#  C1     A1  N1  V1     A2 N2      V2
-def randomSentenceType2(words, verbs, conjunctions):
+# "Mentre il cane scontroso mangia la pecora triste corre"
+#  C1     A1  N1  Q1         V1     A2 N2     Q2     (V2)
+# V2 presente solo se V1 e' intranitivo. Altrimenti N2
+# e' complemento oggetto
+def randomSentenceType2(words, verbs, conjunctions, adjectives):
 
     # Selezione casuale congiunzione
     C1_dict = r.choice(conjunctions)
@@ -94,12 +104,18 @@ def randomSentenceType2(words, verbs, conjunctions):
     N1 = N1_dict['Parola']
     genre = N1_dict['GenereNumero']
 
+    # Qualificazione nome con aggettivo
+    Q_filtered = [q for q in adjectives if (q['GenereNumero'] == genre)]
+    Q1 = r.choice(Q_filtered)['Parola']
+    Q_filtered.clear()
+
     # Selezione filtrata da genere e numero articolo
     A1 = randomArticle(genre, N1)
 
     # Selezione filtrata dei verbi in accordo a sostantivo
     V_filtered = [v for v in verbs if (v['GenereNumero'][0] == genre[0])]
-    V1 = r.choice(V_filtered)['Parola']
+    V1_dict = r.choice(V_filtered)
+    V1 = V1_dict['Parola']
     V_filtered.clear()
 
     # Selezione casuale nome
@@ -107,26 +123,36 @@ def randomSentenceType2(words, verbs, conjunctions):
     N2 = N2_dict['Parola']
     genre =  N2_dict['GenereNumero']
 
+    # Qualificazione nome con aggettivo
+    Q_filtered = [q for q in adjectives if (q['GenereNumero'] == genre)]
+    Q2 = r.choice(Q_filtered)['Parola']
+    Q_filtered.clear()
+
     # Selezione filtrata da genere e numero articolo
     A2 = randomArticle(genre, N2)
 
-    # Selezione filtrata dei verbi in accordo a sostantivo
-    V_filtered = [v for v in verbs if (v['GenereNumero'][0] == genre[0])]
-    V2 = r.choice(V_filtered)['Parola']
-    V_filtered.clear()
+    # Selezione filtrata dei verbi in accordo a sostantivo se il primo verbo e' intransitivo
+    if (V1_dict['Transitivo'] == 'v.intr.'):
+        V_filtered = [v for v in verbs if (v['GenereNumero'][0] == genre[0])]
+        V2 = r.choice(V_filtered)['Parola']
+        V_filtered.clear()
+    else:
+        V2=""
 
     # Costruzione casuale frase in base a struttura
-    return(assemble(C1,A1,N1,V1,A2,N2,V2))
+    return(assemble(C1,A1,N1,Q1,V1,A2,N2,Q2,V2))
 
 ### INIZIO PROGRAMMA ###
 Words = []
 Verbs = []
 Conjunctions = []
+Adjectives = []
 
 # Recuperiamo i dati dai file esterni
 readData(Words, 'data/nomi.csv')
 readData(Verbs, 'data/verbi.csv')
 readData(Conjunctions, 'data/congiunzioni.csv')
+readData(Adjectives, 'data/aggettivi.csv')
 
 # Funzione per ripetizione ciclica generazione
 # frasi casuali. Il tipo di frase e' a sua volta
@@ -134,9 +160,10 @@ readData(Conjunctions, 'data/congiunzioni.csv')
 def generate(nbr_of_sentences):
     for i in range(nbr_of_sentences):
         if r.randint(1,2) == 1:
-            print(randomSentenceType1(Words, Verbs))
+            print(randomSentenceType1(Words, Verbs, Adjectives))
         else:
-            print(randomSentenceType2(Words, Verbs, Conjunctions))
+            print(randomSentenceType2(Words, Verbs, Conjunctions, Adjectives))
 
-# Eseguiamo la generazione
+# Eseguiamo la generazione di frasi casuali con diversa struttura
 generate(100)
+
